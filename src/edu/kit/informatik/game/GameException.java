6a23926f357cc8a05bd6530f12fd3861ad499b42
Max Lohrmann <mlohrmann11@gmail.com>
2023-03-01 16:43:57 +0100

    javadoc

--- src/edu/kit/informatik/game/GameException.java
+++ src/edu/kit/informatik/game/GameException.java
@@ -1,13 +1,22 @@
 package edu.kit.informatik.game;
 
-// TODO Plagiat kennzeichen
+/**
+ * Represents an exception that occur in queens farming.
+ * The implementation was inspired by the sample solution of task 5.
+ * 
+ * @author uiljo
+ * @author Thomas Weber
+ * @version 1.0
+ */
 public class GameException extends IllegalArgumentException {
+
     private static final long serialVersionUID = -97485091L;
 
-    public GameException() {
-        super();
-    }
-
+    /**
+     * Instantiates a new {@link GameException} with a given message.
+     * 
+     * @param message The error message of the exception.
+     */
     public GameException(final String message) {
         super(message);
     }
